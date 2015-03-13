eb8907c8b3c6d5a048e58247d9c2abb026a7340b
Tim Long <Tim@tigranetworks.co.uk>
2015-03-13 06:00:47 +0000

    Implemented a ConsoleExporter, which dumps the contents of the HorizonData collection out to the console.

---
+++ TA.Horizon/Exporters/ConsoleExporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TA.Horizon.Exporters
+    {
+    class ConsoleExporter : IHorizonExporter
+        {
+        public void ExportHorizon(HorizonData data)
+            {
+            Console.WriteLine("Azimuth\tHorizon\tLight Dome");
+            foreach (var azimuth in data)
+                {
+                Console.WriteLine("{0}\t{1:F1}\t{2:F1}", 
+                    azimuth.Key, azimuth.Value.HorizonAltitude,
+                    azimuth.Value.LightDomeAltitude);
+                }
+            }
+
+        public void ProcessCommandLineArguments(string[] args)
+            {
+            // There are currently no command line arguments for the console exporter
+            }
+        }
+    }
