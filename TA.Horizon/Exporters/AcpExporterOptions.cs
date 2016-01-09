56df6d69c4fdb8b49586598d1afa6475a9f5a4d6
Tim Long <Tim@tigranetworks.co.uk>
2016-01-09 16:53:10 +0000

    Move the --UseLightDome option into the AcpExporter and modified teh exporter to include the light dome data when the option is active.

---
+++ TA.Horizon/Exporters/AcpExporterOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace TA.Horizon.Exporters
+    {
+    class AcpExporterOptions
+        {
+        /// <summary>
+        ///     Gets or sets a value indicating whether light dome data (if present) should be used
+        ///     when exporting the horizon. 
+        /// </summary>
+        /// <value><c>true</c> if [use light dome]; otherwise, <c>false</c>.</value>
+        [Option('l', "UseLightDome", DefaultValue = false,
+            HelpText = "Includes light dome data (if present) in the exported horizon.")]
+        public bool UseLightDome { get; set; }
+        }
+    }
