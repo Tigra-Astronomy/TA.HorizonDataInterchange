186bae9b37e17cf5e6b581a6e9b5d923fb6a0598
David W. Wormuth <dwormuth@post.harvard.edu>
2016-07-23 19:22:53 -0400

    Copied AstroplannerImporter AstroplannerOption to ObserverPro

---
+++ TA.Horizon/Importers/ObserverProImporter.cs
@@ -0,0 +1,68 @@
+// This file is part of the TA.Horizon project
+// 
+// Copyright © 2015 Tigra Networks., all rights reserved.
+// 
+// File: AstroPlannerImporter.cs  Last modified: 2015-03-08@01:43 by Tim Long
+
+using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using CommandLine;
+
+namespace TA.Horizon.Importers
+    {
+    internal class AstroPlannerImporter : IHorizonImporter
+        {
+        Stream source;
+        string[] commandLineArguments;
+        ParserResult<AstroplannerOptions> options;
+
+        public AstroPlannerImporter(){}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AstroPlannerImporter"/> class and directly sets the source stream.
+        /// This constructor is only available internally and is intended for unit testing.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        internal AstroPlannerImporter(Stream source)
+            {
+            this.source = source;
+            }
+
+        public HorizonData ImportHorizon()
+            {
+            var horizonData = new HorizonData();
+            using (var reader = new StreamReader(source))
+                {
+                reader.ReadLine(); // Skip the header line: Azimuth,Lower,Light Dome
+                while (!reader.EndOfStream)
+                    {
+                    var sourceLine = reader.ReadLine();
+                    var parts = sourceLine.Split(',');
+                    if (parts.Length != 3) 
+                        throw new FormatException("Unable to parse input file (missing fields)");
+                    var azimuth = int.Parse(parts[0]);
+                    var horizon = double.Parse(parts[1]);
+                    var lightDome = double.Parse(parts[2]);
+                    horizonData[azimuth] = new HorizonDatum(horizon, lightDome);
+                    }
+                }
+            return horizonData;
+            }
+
+        public void ProcessCommandLineArguments(Parser parser, string[] args)
+            {
+            this.commandLineArguments = args;
+           
+            options = parser.ParseArguments<AstroplannerOptions>(args);
+            if (options.Errors.Any())
+                {
+                Environment.ExitCode = -1;
+                throw new ArgumentException("An error occurred processing the command line options.");
+                }
+
+            source = new FileStream(options.Value.SourceFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+            }
+        }
+    }
